970bb5dde417da54a94761dd2c751308420e978a
vitaovich <vitaliyalekhnovich@gmail.com>
2024-01-28 14:18:41 +0000

    add containers and role assignment

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -37,6 +37,18 @@ resource "azurerm_storage_account" "models" {
   }
 }
 
+resource "azurerm_storage_container" "modified" {
+  name                  = "modified-${substr(sha256(azurerm_resource_group.rg.name),0,13)}"
+  storage_account_name  = azurerm_storage_account.models.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "original" {
+  name                  = "original-${substr(sha256(azurerm_resource_group.rg.name),0,13)}"
+  storage_account_name  = azurerm_storage_account.models.name
+  container_access_type = "private"
+}
+
 resource "azurerm_storage_account" "azfunc" {
   name                     = "${substr(sha256(azurerm_resource_group.rg.name),0,13)}azfunc"
   resource_group_name      = azurerm_resource_group.rg.name
@@ -50,3 +62,8 @@ resource "azurerm_storage_account" "azfunc" {
   }
 }
 
+resource "azurerm_role_assignment" "storage_contributor" {
+  scope                = azurerm_storage_account.models.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = "22a14f43-816f-4fc8-8b17-08b3769ce9de"
+}
